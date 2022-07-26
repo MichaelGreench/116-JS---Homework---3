70c2934bda7032a70d6c0cd70683df68e17a6b65
MichaelGreench <106013652+MichaelGreench@users.noreply.github.com>
2022-07-26 13:55:59 +0400

    116-JS---Homework---3

---
+++ script.js
@@ -0,0 +1,92 @@
+
+    // დავალება №1
+
+    // function sumPos(number){
+    //    let neg = [];
+    //     let sum = 0;
+    // for (let i = 0; i < number.length; i++) {
+    //     if(number[i] < 0) {
+    //         neg.push(number[i]);
+    //     }
+    //     else{
+    //         sum += number [i];
+    //     } 
+    // }
+    //     return sum;  
+    // }
+
+    //     let sum_result = sumPos([2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8])
+
+    //     console.log(sum_result);
+
+
+    // დავალება №2
+
+    //     function sum(numbers) {
+    //         let sum = 0;
+    //         for( let i = 0; i < numbers.length; i++) {
+    //             sum += numbers[i];
+    //         }
+
+    //         return sum;
+    //     }
+
+    //         let summTotal = sum([10, 50, 6, 7 , 8 , 11, 6, 3, 9]);
+
+    //         console.log(summTotal);
+    
+    // დავალება №3
+
+    //     let user= {
+    //         firstname: 'giorgi',
+    //         lastname: 'saakadze',
+    //         age: 32,
+    //         isloggedin: true
+    //     }
+
+    //     if(isloggedin = 'true') {
+    //             console.log('giorgi'+' '+'saakadze');
+    //     }
+    //     else{
+    //             console.log('false');
+    //     }
+
+    // დავალება №4
+
+    //     function searchMax(maximum) {
+    //          let max = 0;
+    //          for (let i = 0; i < maximum.length; i++) {
+    //          if(maximum[i] > max) {
+    //             max = maximum[i];
+    //      }
+
+    //         return max;
+    //     }
+    // }
+
+    // let maxSearcher = searchMax ([1, 11, 12, 15, 17, 101, 1001]);
+    // console.log(maxSearcher);
+
+    // დავალება №5
+
+    //         let numbers = [1, 2, 4, 10, 34, 5, 7, 87];
+    //         for(let i = 0; i < numbers.length; i++) {
+    //             if(numbers[i] > 0 && numbers[i] < 10) {
+    //                 console.log(numbers[i]);
+    //             }
+    //         }
+
+    // დავალება №6
+    
+    //         let array = [1, 2, 4, 5, 6, 7, 8, 9, 10];
+    //         for (let i = 0; i < array.length; i++) {
+    //             if (array[i] === 5) {
+    //                 break;
+    //             }
+                
+    //             console.log(array[i]);
+    //         }
+
+            
+            
+            
